a81955edffe0c23eb484b69eeae2aef971518e1a
kato-hk28 <83061468+kato-hk28@users.noreply.github.com>
2021-09-26 01:14:29 +0900

    メリーさんをマップに表示

--- mary_call/lib/main.dart
+++ mary_call/lib/main.dart
@@ -4,9 +4,11 @@ import 'dart:async';
 import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'addTimer.dart';
 import 'variant.dart';
+import 'map.dart';
 
 void main() {
   runApp(MaterialApp(home: TimerPage())); // ここが Widget ツリーの起点
@@ -22,23 +24,46 @@ class _TimerPageState extends State<TimerPage> {
   Timer _timer; // この辺が状態
   DateTime _time;
   DateTime set_time;
+  String _location = "no data";
+  Future<void> getLocation() async {
+    // 現在の位置を返す
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    // 北緯がプラス。南緯がマイナス
+    print("緯度: " + position.latitude.toString());
+    // 東経がプラス、西経がマイナス
+    print("経度: " + position.longitude.toString());
+    // 高度
+    print("高度: " + position.altitude.toString());
+    // 距離をメートルで返す
+    double distanceInMeters =
+        Geolocator.distanceBetween(35.68, 139.76, -23.61, -46.40);
+    print(distanceInMeters);
+    // 方位を返す
+    double bearing = Geolocator.bearingBetween(35.68, 139.76, -23.61, -46.40);
+    print(bearing);
+    setState(() {
+      _location = position.toString();
+    });
+  }
 
   //DateTime.parse(2019-04-30 10:48:27.701406)
   //2019-04-30 10:48:27.7014
   void _onTimer(Timer timer) {
-    print("counting");
+    // print("counting");
     var now = DateTime.now();
     now = now.add(Duration(hours: 9));
-    print(now.toIso8601String());
+    // print(now.toIso8601String());
     if (set_times != null) {
       set_time = set_times[0];
-      print(set_time.toIso8601String());
+      // print(set_time.toIso8601String());
       var diff = set_time.difference(now).inSeconds.abs();
-      print(diff);
+      // print(diff);
       if (diff < 1) {
         FlutterRingtonePlayer.playAlarm();
         set_times.removeAt(0);
         print("start");
+        getLocation();
       }
     } else {}
   }
@@ -49,7 +74,7 @@ class _TimerPageState extends State<TimerPage> {
     // set_times.add(DateTime.parse("2021-09-25 14:40:30.701406"));
 
     _time = DateTime.utc(0, 0, 0);
-    Timer.periodic(const Duration(seconds: 1), _onTimer);
+    // Timer.periodic(const Duration(seconds: 1), _onTimer);
     super.initState();
   }
 
@@ -58,47 +83,38 @@ class _TimerPageState extends State<TimerPage> {
     // setState() の度に実行される
     return Scaffold(
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(
-          DateFormat.Hms().format(_time),
-          style: Theme.of(context).textTheme.headline2,
-        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FloatingActionButton(
+              heroTag: "hero1",
               onPressed: () {
                 FlutterRingtonePlayer.stop();
                 print("stop");
                 // Stopボタンタップ時の処理
-                if (_timer != null && _timer.isActive) _timer.cancel();
               },
               child: Text("Stop"),
             ),
             FloatingActionButton(
+              heroTag: "hero2",
               onPressed: () {
-                // Timer.periodic(const Duration(seconds: 1), _onTimer);
-                FlutterRingtonePlayer.playAlarm();
-                print("start");
-                // Startボタンタップ時の処理
-                // _timer = Timer.periodic(
-                //   Duration(seconds: 1), // 1秒毎に定期実行
-                //   (Timer timer) {
-                //     setState(() {
-                //       // 変更を画面に反映するため、setState()している
-                //       _time = _time.add(Duration(seconds: 1));
-                //     });
-                //   },
-                // );
+                print("get location");
+                // Stopボタンタップ時の処理
+                getLocation();
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MapPage()));
               },
-              child: Text("Start"),
+              child: Text("get location"),
             ),
           ],
         )
       ]),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => addTimer()));
-      }),
+      floatingActionButton: FloatingActionButton(
+          heroTag: "hero3",
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => addTimer()));
+          }),
     );
   }
 }
